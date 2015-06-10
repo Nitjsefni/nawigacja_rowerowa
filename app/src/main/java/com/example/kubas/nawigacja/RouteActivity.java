5175ecc80d20be830f99f6586d4cfc8e6f7f5d62
Nitjsefni <jakub.sobieski@ingonyama.pl>
2015-06-10 22:08:34 +0200

    Dodanie wyswietlania predkosci i prawidlowych znakow manewrow 2

--- app/src/main/java/com/example/kubas/nawigacja/RouteActivity.java
+++ app/src/main/java/com/example/kubas/nawigacja/RouteActivity.java
@@ -6,9 +6,9 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.kubas.nawigacja.client.OwnOSRMRoadManager;
 import com.example.kubas.nawigacja.data.model.GeoPosition;
@@ -19,7 +19,6 @@ import com.example.kubas.nawigacja.tracking.Trackable;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.bonuspack.overlays.Marker;
 import org.osmdroid.bonuspack.overlays.Polyline;
-import org.osmdroid.bonuspack.routing.OSRMRoadManager;
 import org.osmdroid.bonuspack.routing.Road;
 import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.bonuspack.routing.RoadNode;
@@ -37,11 +36,20 @@ public class RouteActivity extends Activity implements Trackable {
     private ShowPosition showPosition;
     Polyline roadOverlay;
     IMapController mapController;
+    private ImageView maneuverImg;
+    private TextView txtV_Route_DistanceNode, txtV_Route_InstructionNode, txtV_Route_TimeNode, txtV_Route_Speed,txtV_Route_Time, txtV_Route_MaxLength;
+    private double maxLength=0.0, maxDuration=0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_route);
-
+        maneuverImg = (ImageView) findViewById(R.id.maneuverImg);
+        txtV_Route_DistanceNode = (TextView) findViewById(R.id.txtV_Route_DistanceNode);
+        txtV_Route_TimeNode = (TextView) findViewById(R.id.txtV_Route_TimeNode);
+        txtV_Route_InstructionNode = (TextView) findViewById(R.id.txtV_Route_InstructionNode);
+        txtV_Route_Speed = (TextView) findViewById(R.id.txtV_Route_Speed);
+        txtV_Route_Time = (TextView) findViewById(R.id.txtV_Route_Time);
+        txtV_Route_MaxLength = (TextView) findViewById(R.id.txtV_Route_MaxLength);
         map = (MapView) findViewById(R.id.map2);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
@@ -118,8 +126,10 @@ public class RouteActivity extends Activity implements Trackable {
                             roadOverlay = RoadManager.buildRoadOverlay(road, Color.RED, 8, RouteActivity.this);
                             map.getOverlays().add(roadOverlay);
                             TypedArray iconIds = getResources().obtainTypedArray(R.array.direction_icons);
-
-
+                            String lenght= getLengthText(road.mLength);
+                            txtV_Route_MaxLength.setText(lenght);
+                            String duration= getDurationText(road.mDuration);
+                            txtV_Route_MaxLength.setText(duration);
                             for (int i = 0; i < road.mNodes.size(); i++) {
                                 RoadNode node = road.mNodes.get(i);
                                 Marker nodeMarker = new Marker(map);
@@ -128,6 +138,7 @@ public class RouteActivity extends Activity implements Trackable {
                                 nodeMarker.setTitle("Step " + i);
                                 nodeMarker.setSnippet(node.mInstructions);
                                 nodeMarker.setSubDescription(Road.getLengthDurationText(node.mLength, node.mDuration));
+
                                 Drawable icon = getResources().getDrawable(R.drawable.ic_continue);
                                 int iconId = iconIds.getResourceId(node.mManeuverType, R.drawable.ic_empty);
                                 if (iconId != R.drawable.ic_empty){
@@ -154,10 +165,47 @@ public class RouteActivity extends Activity implements Trackable {
 
 
     }
+    public String getLengthText(double length){
+        String result;
+        if (length >= 100.0){
+            result = (int)(length) + " km";
+        } else if (length >= 1.0){
+            result = Math.round(length*10)/10.0 + " km";
+        } else {
+            result = (int)(length*1000) + " m";
+        }
+
+        return result;
+    }
+    public String getDurationText(double duration){
+        String result="";
+
+        int totalSeconds = (int)duration;
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds / 60) - (hours*60);
+        int seconds = (totalSeconds % 60);
+        if (hours != 0){
+            result += hours + " h";
+        }
+        if (minutes != 0){
+            result += minutes + " min";
+        }
+        if (hours == 0 && minutes == 0){
+            result += seconds + " s";
+        }
+        return result;
+    }
 
 
     public void refreshMapPosition(Location loc) {
         GeoPoint currentLocation = new GeoPoint(loc);
+        if(loc.hasSpeed()) {
+            txtV_Route_Speed.setText(String.valueOf(loc.getSpeed()) + " m/s");
+        }
+        else
+        {
+            txtV_Route_Speed.setText("0 m/s");
+        }
         mapController.setCenter(currentLocation);
         Marker startMarker = new Marker(map);
         startMarker.setPosition(currentLocation);
