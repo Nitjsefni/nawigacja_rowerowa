b605cd8854ca36bf02f8749335e566fb4c767e80
Krzysztof_Pawlak <KPawlak@wolterskluwer.pl>
2015-06-10 15:56:44 +0200

    zmiany

--- app/src/main/java/com/example/kubas/nawigacja/client/OwnOSRMRoadManager.java
+++ app/src/main/java/com/example/kubas/nawigacja/client/OwnOSRMRoadManager.java
@@ -1,9 +1,22 @@
 package com.example.kubas.nawigacja.client;
 
 import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.util.GeoPoint;
+
+import java.util.ArrayList;
 
 public class OwnOSRMRoadManager extends OSRMRoadManager {
     public OwnOSRMRoadManager() {
         setService(ServerAddress.getServerUrl() + "webservices/viaroute?");
+    }
+
+    @Override
+    public Road getRoad(ArrayList<GeoPoint> waypoints) {
+        Road road = super.getRoad(waypoints);
+        for (GeoPoint point : road.mRouteHigh) {
+            point.setCoordsE6(point.getLatitudeE6() * 10, point.getLongitudeE6() * 10);
+        }
+        return road;
     }
 }
